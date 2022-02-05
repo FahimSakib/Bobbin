41b5a086af720e12065faa4d6c12daed74c38b42
Istiak60 <istisquare@gmail.com>
2022-02-05 12:25:19 +0600

    cart_page_mastering_of_frontend_template_done

---
+++ resources/views/frontend/pages/cart/sections/container.blade.php
@@ -0,0 +1,5 @@
+<div class="page-header text-center" style="background-image: url('asset/frontend/assets/images/page-header-bg.jpg')">
+        		<div class="container">
+        			<h1 class="page-title">Shopping Cart<span>Shop</span></h1>
+        		</div><!-- End .container -->
+        	</div><!-- End .page-header -->
