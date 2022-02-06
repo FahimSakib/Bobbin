6f6db9904e79db5d404620b25e5621fa71f0e0f7
Istiak60 <istisquare@gmail.com>
2022-02-07 02:02:09 +0600

    sidebar_updated

--- resources/views/backend/include/sidebar.blade.php
+++ resources/views/backend/include/sidebar.blade.php
@@ -20,6 +20,15 @@
             </li>
             <li class="dropdown">
                 <a href="#" class="menu-toggle nav-link has-dropdown"><i
+                        data-feather="shopping-bag"></i><span>Products</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="widget-chart.html">Uploads</a></li>
+                    <li><a class="nav-link" href="widget-data.html">View</a></li>
+                    <li><a class="nav-link" href="widget-data.html">Edit</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="menu-toggle nav-link has-dropdown"><i
                         data-feather="command"></i><span>Apps</span></a>
                 <ul class="dropdown-menu">
                     <li><a class="nav-link" href="chat.html">Chat</a></li>
