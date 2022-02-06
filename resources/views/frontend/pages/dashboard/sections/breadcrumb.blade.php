36d2e862a062b79e00addca6632ab5fbfad4cec3
Arfan A. Khan <73901113+ArfanGits@users.noreply.github.com>
2022-02-06 21:53:58 +0600

    dashboard done

---
+++ resources/views/frontend/pages/dashboard/sections/breadcrumb.blade.php
@@ -0,0 +1,9 @@
+<nav aria-label="breadcrumb" class="breadcrumb-nav mb-3">
+    <div class="container">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+            <li class="breadcrumb-item"><a href="#">Shop</a></li>
+            <li class="breadcrumb-item active" aria-current="page">My Account</li>
+        </ol>
+    </div><!-- End .container -->
+</nav><!-- End .breadcrumb-nav -->
