14e1ff56b4be89a4a9ff4ea69f21edb380faef62
FahimSakib <fsakib991@gmail.com>
2022-02-27 15:17:11 +0600

    product images upload

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -60,13 +60,35 @@ class ProductController extends Controller
             'qty'               => 'required|numeric',
             'short_description' => 'required',
             'description'       => 'required',
+            'image1'            => 'required|image|mimes:png,jpeg,jpg',
+            'image2'            => 'required|image|mimes:png,jpeg,jpg',
+            'image3'            => 'required|image|mimes:png,jpeg,jpg',
+            'image4'            => 'required|image|mimes:png,jpeg,jpg',
             'category_id'       => 'required',
             'status'            => 'required',
             'size_id'           => 'required',
             'color_id'          => 'required'
         ]);
 
+        $file =  $request->file('image1');
+        $uploadName1 = $this->fileUpload($file,'image1');
+        
+        $file =  $request->file('image2');
+        $uploadName2 = $this->fileUpload($file,'image2');
+
+        $file =  $request->file('image3');
+        $uploadName3 = $this->fileUpload($file,'image3');
+
+        $file =  $request->file('image4');
+        $uploadName4 = $this->fileUpload($file,'image4');
+
+
         $product = new Product($request->except('size_id', 'color_id'));
+
+        $product->image1 = $uploadName1;
+        $product->image2 = $uploadName2;
+        $product->image3 = $uploadName3;
+        $product->image4 = $uploadName4;
 
         if ($product->save()) {
             $sizes = $request->size_id;
@@ -128,5 +150,17 @@ class ProductController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    private function fileUpload($file, $name){
+        $prefix='Product_'.time().'_';
+        $picture='';
+        if(!empty($file)){
+            $name=$name.'_img.';
+            $fileext = $file->getClientOriginalExtension();
+            $picture = $prefix.$name.$fileext;
+            $path = $file->storeAs('public/Product_Image',$picture);
+        }
+        return $picture;
     }
 }
