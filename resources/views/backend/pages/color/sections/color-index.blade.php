f634187aa3da4b55758b1262a47e5b1df092bbe4
Istiak60 <istisquare@gmail.com>
2022-02-28 02:40:32 +0600

    modified_design

--- resources/views/backend/pages/color/sections/color-index.blade.php
+++ resources/views/backend/pages/color/sections/color-index.blade.php
@@ -1,18 +1,47 @@
 <!-- Main Content -->
 <div class="main-content">
     <section class="section">
-        <h2>Colors</h2>
         <div class="section-body">
             <div class="row">
                 <div class="col-12">
                     <div class="card">
-                        <div class="card-header">
-                            
-                              <div class="pull-left " >
-                        <a class="btn btn-icon icon-left btn-warning" href="{{ route('admin.color.create') }}"><i class="fas fa-plus-circle"></i>Create An color</a>
-                    </div>
+                        <div class="card-header row">
+                            <div class="col-md-10 d-flex mt-2">
+                                <div class="preview d-flex">
+                                    <div class="icon-preview" style="margin-top: 2px;">
+                                        <i class="fas fa-table"></i>
+                                    </div>
+                                    <div class="icon-class" style="font-size: 25px;">Index of Colors</div>
+                                </div>
+                            </div>
+                            <div class="pull-right">
+                                <a class="btn btn-icon icon-left btn-warning"
+                                    href="{{ route('admin.color.create') }}"><i class="fas fa-plus-circle"></i>Create
+                                    An color</a>
+                            </div>
                         </div>
                         <div class="card-body">
+                            @if ($message = Session::get('success'))
+                            <div class="alert alert-success alert-dismissible show fade">
+                                <div class="alert-body">
+                                    <button class="close" data-dismiss="alert">
+                                        <span>&times;</span>
+                                    </button>
+                                    {{ $message }}
+                                </div>
+                            </div>
+                            @endif
+                            @if ($message = Session::get('danger'))
+                            <div class="alert alert-danger alert-dismissible show fade">
+                                <div class="alert-body">
+                                    <button class="close" data-dismiss="alert">
+                                        <span>&times;</span>
+                                    </button>
+                                    {{ $message }}
+                                </div>
+                            </div>
+                            @endif
+                            @if (!$color->isEmpty())
                             <div class="table-responsive">
                                 <table class="table table-striped" id="table-1">
                                     <thead>
@@ -50,19 +79,20 @@
                                                     <div class="dropdown-menu">
                                                         <a class="dropdown-item has-icon"
                                                             href="{{ route('admin.color.show',$item->id) }}"><i
-                                                                class="far fa-eye"></i>   View</a>
+                                                                class="far fa-eye"></i> View</a>
                                                         <a class="dropdown-item has-icon"
                                                             href="{{ route('admin.color.edit',$item->id) }}"><i
-                                                                class="far fa-edit"></i>   Edit</a>
-                                                               <div class="del ml-4"> 
+                                                                class="far fa-edit"></i> Edit</a>
+                                                        <div class="del ml-4">
                                                             <form action="{{ route('admin.color.destroy',$item->id) }}"
-                                                            method="POST">
-                                                            @csrf
-                                                            @method('DELETE')
-                                                            <i class="fas fa-trash-alt"></i> <button type="submit"
-                                                                class="btn" aria-hidden="true"  style="background-color:transparent; margin-right:50px;">Delete</button>
-                                                        </form>
-                                                       </div>
+                                                                method="POST">
+                                                                @csrf
+                                                                @method('DELETE')
+                                                                <i class="fas fa-trash-alt"></i> <button type="submit"
+                                                                    class="btn" aria-hidden="true"
+                                                                    style="background-color:transparent; margin-right:50px;">Delete</button>
+                                                            </form>
+                                                        </div>
 
                                                     </div>
                                                 </div>
@@ -72,9 +102,44 @@
                                     </tbody>
                                 </table>
                             </div>
+                            @else
+                            <div class="row justify-content-center">
+                                <div class="col-md-12">
+                                    <p class="text-center">
+                                        <img src="asset/backend/assets/img/created/open-box.png" alt="empty-box"
+                                            style="width: 300px;height:300px">
+                                    </p>
+                                </div>
+                                <div class="alert alert-info alert-has-icon col-md-8">
+                                    <div class="alert-icon"><i class="fa fa-2x fa-lightbulb"></i></div>
+                                    <div class="alert-body">
+                                        <div class="alert-title">
+                                            <h3>Info</h3>
+                                        </div>
+                                        <h5>
+                                            There is no color available! please create a new color to show in the
+                                            list.
+                                        </h5>
+                                    </div>
+                                </div>
+                                @endif
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
     </section>
     @include('backend.include.setting-sidebar')
+    @push('style')
+    <link rel="stylesheet" href="asset/backend/assets/bundles/datatables/datatables.min.css">
+    <link rel="stylesheet"
+        href="asset/backend/assets/bundles/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css">
+    @endpush
+
+    @push('script')
+    <!-- JS Libraies -->
+    <script src="asset/backend/assets/bundles/datatables/datatables.min.js"></script>
+    <script src="asset/backend/assets/bundles/datatables/DataTables-1.10.16/js/dataTables.bootstrap4.min.js"></script>
+    <script src="asset/backend/assets/bundles/jquery-ui/jquery-ui.min.js"></script>
+    <!-- Page Specific JS File -->
+    <script src="asset/backend/assets/js/page/datatables.js"></script>
+    @endpush
--- resources/views/backend/pages/color/sections/color-show.blade.php
+++ resources/views/backend/pages/color/sections/color-show.blade.php
@@ -1,55 +1,63 @@
 <!-- Main Content -->
 <div class="main-content">
     <section class="section">
-        <h2>Color details</h2>
         <div class="section-body">
             <div class="row">
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                            
-                              <div class="pull-left " >
-                        <a class="btn btn-icon icon-left btn-success" href="{{ route('admin.color.index') }}"><i class="fas fa-list-alt"></i>List of Color</a>
-                    </div>
-                      </div>
+                            <div class="col-md-10 d-flex mt-2">
+                                <div class="preview d-flex">
+                                    <div class="icon-preview" style="margin-top: 2px;">
+                                        <i class="fas fa-info-circle"></i>
+                                    </div>
+                                    <div class="icon-class" style="font-size: 25px;">Color Details</div>
+                                </div>
+                            </div>
+                            <div>
+                                <a class="btn btn-icon icon-left btn-success" href="{{ route('admin.color.index') }}"><i
+                                        class="fas fa-list-alt"></i>List of
+                                    colors</a>
+                            </div>
+                        </div>
                         <div class="card-body">
                             <div class="table-responsive">
-                               <table class="table table-bordered">
-                                  <tbody>                 
-                                      <tr>
-                                          <th>Fields</th>
-                                          <th>Details</th>
-                                      </tr>
-                                      <tr>
-                                          <th>ID</th>
-                                          <td>{{ $color->id }}</td>
-                                      </tr>
-                                      <tr>
-                                          <th>Title</th>
-                                          <td>{{ $color->title }}</td>
-                                      </tr>
-                                      <tr>
-                                          <th>Status</th>
-                                         
-                                            @if($color->status=='1')                               
-                                                <td>Active</td>
-                                            @else                               
-                                                <td>Deactive</td>
+                                <table class="table table-bordered">
+                                    <tbody>
+                                        <tr>
+                                            <th>Fields</th>
+                                            <th>Details</th>
+                                        </tr>
+                                        <tr>
+                                            <th>ID</th>
+                                            <td>{{ $color->id }}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Title</th>
+                                            <td>{{ $color->title }}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Status</th>
+
+                                            @if($color->status=='1')
+                                            <td>Active</td>
+                                            @else
+                                            <td>Deactive</td>
 
                                             @endif
-                                      </tr>
-                                     
-                                      <tr>
-                                          <th>Created at</th>
-                                          {{-- <td>{{ $color->created_at}}</td> --}}
-                                          <td>{!! date('d - M - Y', strtotime($color->created_at)) !!}</td>
-                                      </tr>
-                                      <tr>
-                                          <th>Updated at</th>
-                                          <td>{!! date('d - M - Y', strtotime($color->updated_at)) !!}</td>
-                                      </tr>
-                                  </tbody>
-                              </table>
+                                        </tr>
+
+                                        <tr>
+                                            <th>Created at</th>
+                                            {{-- <td>{{ $color->created_at}}</td> --}}
+                                            <td>{!! date('d - M - Y - h : i : s A', strtotime($color->created_at)) !!}
+                                        </tr>
+                                        <tr>
+                                            <th>Updated at</th>
+                                            <td>{!! date('d - M - Y - h : i : s A', strtotime($color->created_at)) !!}
+                                        </tr>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
@@ -57,3 +65,4 @@
             </div>
     </section>
     @include('backend.include.setting-sidebar')
+
