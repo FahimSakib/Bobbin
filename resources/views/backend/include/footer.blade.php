3270857efbc536ef3f0da414c4e338c8b6d87b8f
FahimSakib <fsakib991@gmail.com>
2022-03-01 19:34:39 +0600

    footer removed

--- resources/views/backend/include/footer.blade.php
+++ resources/views/backend/include/footer.blade.php
@@ -1,7 +1,3 @@
-<footer class="main-footer">
-    <div class="footer-left">
-        <a href="templateshub.net">Templateshub</a></a>
-    </div>
-    <div class="footer-right">
-    </div>
-</footer>
+{{-- <footer class="main-footer">
+    add your footer here
+</footer> --}}
