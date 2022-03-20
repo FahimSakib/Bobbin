85854c9d179f97eef198b9bc4e3c05c58abc4b42
FahimSakib <fsakib991@gmail.com>
2022-03-20 13:21:39 +0600

    Product Extended Description you may also like

--- app/Http/Controllers/Frontend/ProductextendedController.php
+++ app/Http/Controllers/Frontend/ProductextendedController.php
@@ -12,7 +12,11 @@ class ProductextendedController extends Controller
         $data = [
             'title' => 'Product'
         ];
+
         $product = Product::with('category')->find($id);
-        return view('frontend.pages.product-extended.product-extended', $data,compact('product'));
+
+        $prducts_random = Product::with('category')->inRandomOrder()->limit(6)->get();
+
+        return view('frontend.pages.product-extended.product-extended', $data,compact('product','prducts_random'));
     }
 }
