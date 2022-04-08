aa06bf8b405cb4f81d3ed026d035c4edb517a39b
Istiak60 <istisquare@gmail.com>
2022-04-08 06:09:45 +0600

    google_map_location_added

--- resources/views/frontend/pages/contact/sections/page-content.blade.php
+++ resources/views/frontend/pages/contact/sections/page-content.blade.php
@@ -149,5 +149,8 @@
             </div><!-- End .row -->
         </div><!-- End .stores -->
     </div><!-- End .container -->
-    <div id="map"></div><!-- End #map -->
+    {{-- <div id="map">
+    </div><!-- End #map --> --}}
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d230.65688929179183!2d91.83363417334833!3d22.334355354066105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30acd9f7b142e911%3A0x919ca40ed9ece9d!2sBobbin!5e0!3m2!1sen!2sbd!4v1649376418672!5m2!1sen!2sbd" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    
 </div><!-- End .page-content -->
