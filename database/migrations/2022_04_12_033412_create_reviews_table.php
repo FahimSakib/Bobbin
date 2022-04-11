11f0eabaf6b4585047d81408972b3a6b295e3584
Istiak60 <istisquare@gmail.com>
2022-04-12 05:08:09 +0600

    review_

---
+++ database/migrations/2022_04_12_033412_create_reviews_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->integer('rating')->default(0);
+            $table->string('user_name');
+            $table->string('product_name');
+            $table->string('review');
+            $table->enum('status',['0','1'])->default('1')->comment="0=Inactive,1=Active";
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
