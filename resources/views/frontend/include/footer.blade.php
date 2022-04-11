9af6d663c2182ab51a68b393aad09f71ca15b51a
Istiak60 <istisquare@gmail.com>
2022-04-12 01:57:31 +0600

    footer_designed_changed_as_required

--- resources/views/frontend/include/footer.blade.php
+++ resources/views/frontend/include/footer.blade.php
@@ -6,7 +6,8 @@
                      <div class="widget widget-about">
                          {{-- <img src="asset/frontend/assets/images/bobbin-logo-only-gold.png" alt="Bobbin Logo" width="180" height="30"> --}}
                          <p class="footer-brand">Bobbin</p>
-                         <p class="text-justify">Bobbin is an eminent lifestyle brand in the retail fashion industry of Bangladesh with the
+                         <p class="text-justify">Bobbin is an eminent lifestyle brand in the retail fashion industry of
+                             Bangladesh with the
                              purpose of provide qualityfull fashion for all within their range. As a fashion brand,
                              Bobbin is renowned for its unique style and variety of collections. We crafted our
                              fashionable attires & accessories for all age ranges who believe themselves to be stand out
@@ -18,31 +19,30 @@
                                      <span class="widget-about-title">Got Question? Call us 24/7</span>
                                      <a href="tel:01609469623">+880 1609469623</a>
                                  </div><!-- End .col-sm-6 -->
-                                 <div class="col-sm-6 col-md-8">
-                                     {{-- <span class="widget-about-title">Payment Method</span> --}}
-                                     {{-- <figure class="footer-payments">
-                                                <img src="asset/frontend/assets/images/payments.png" alt="Payment methods" width="272"
-                                                    height="20">
-                                            </figure><!-- End .footer-payments --> --}}
-                                 </div><!-- End .col-sm-6 -->
+                                 
+                                 {{-- <div class="col-sm-6 col-md-8">
+                                     <span class="widget-about-title">Payment Method</span>
+                                     <figure class="footer-payments">
+                                         <img src="asset/frontend/assets/images/payments.png" alt="Payment methods"
+                                             width="272" height="20">
+                                     </figure><!-- End .footer-payments -->
+                                 </div><!-- End .col-sm-6 --> --}}
                              </div><!-- End .row -->
+                             <br>
+                             <div class="row">
+                             <div class="col-sm-6 col-md-8">
+                                     <span class="widget-about-title">We Accept</span>
+                                     <figure class="footer-payments">
+                                         <img src="asset/frontend/assets/images/payment-method.png" alt="Payment methods"
+                                             width="272" height="20">
+                                     </figure><!-- End .footer-payments -->
+                                 </div>
+                                 </div><!-- End .row -->
                          </div><!-- End .widget-about-info -->
                      </div><!-- End .widget about-widget -->
                  </div><!-- End .col-sm-12 col-lg-3 -->
 
-                 <div class="col-sm-4 col-lg-2">
-                     <div class="widget">
-                         <h4 class="widget-title">Information</h4><!-- End .widget-title -->
 
-                         <ul class="widget-list">
-                             <li><a href="{{ route('about') }}">About Bobbin</a></li>
-                             <li><a href="#">How to shop on Bobbin</a></li>
-                             <li><a href="{{ route('faq') }}">FAQ</a></li>
-                             <li><a href="{{ route('contact') }}">Contact us</a></li>
-                             {{-- <li><a href="{{ route('login') }}">Log in</a></li> --}}
-                         </ul><!-- End .widget-list -->
-                     </div><!-- End .widget -->
-                 </div><!-- End .col-sm-4 col-lg-3 -->
 
                  {{-- <div class="col-sm-4 col-lg-2">
                             <div class="widget">
@@ -64,15 +64,40 @@
                          <h4 class="widget-title">My Account</h4><!-- End .widget-title -->
 
                          <ul class="widget-list">
-                             <li><a href="{{ route('login') }}">Sign In</a></li>
-                             <li><a href="{{ route('cart') }}">View Cart</a></li>
+                             
                              {{-- <li><a href="#">My Wishlist</a></li>
                                     <li><a href="#">Track My Order</a></li> --}}
-                             <li><a href="#">Help</a></li>
+                                                       @if (auth()->guest())
+                            <li><a href="{{ route('login') }}">Sign In</a></li>
+                             <li><a href="{{ route('cart') }}">View Cart</a></li>
+                            @else
+                            <li class="mr-3"><a href="{{ route('dashboard') }}">Dashboard</a></li>
+                            <li><a href="{{ route('cart') }}">View Cart</a></li>
+                             @endif
+
                          </ul><!-- End .widget-list -->
                      </div><!-- End .widget -->
-                 </div><!-- End .col-sm-64 col-lg-3 -->
+                 </div><!-- End .col-sm-64 col-lg-6 -->
+                 <div class="col-sm-6 col-lg-4">
+                     <div class="widget">
+                         <h4 class="widget-title">Information</h4><!-- End .widget-title -->
+
+                         <ul class="widget-list">
+                             <li><a href="{{ route('about') }}">About Bobbin</a></li>
+                             <li><a href="#">How to shop on Bobbin</a></li>
+                             <li><a href="{{ route('faq') }}">FAQ</a></li>
+                             <li><a href="{{ route('contact') }}">Contact us</a></li>
+                             <li><iframe
+                                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d230.65688929179183!2d91.83363417334833!3d22.334355354066105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30acd9f7b142e911%3A0x919ca40ed9ece9d!2sBobbin!5e0!3m2!1sen!2sbd!4v1649376418672!5m2!1sen!2sbd"
+                                     width="100%" height="250" style="border:0;padding-leftt:100px;" allowfullscreen=""
+                                     loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></li>
+                             {{-- <li><a href="{{ route('login') }}">Log in</a></li> --}}
+                         </ul><!-- End .widget-list -->
+                     </div><!-- End .widget -->
+                 </div><!-- End .col-sm-4 col-lg-3 -->
+
              </div><!-- End .row -->
+
          </div><!-- End .container -->
      </div><!-- End .footer-middle -->
 
@@ -91,8 +116,8 @@
                      target="_blank"><i class="icon-facebook-f"></i></a>
                  {{-- <a href="#" class="social-icon social-twitter" title="Twitter" target="_blank"><i
                          class="icon-twitter"></i></a> --}}
-                 <a href="https://www.instagram.com/bobbin513/?hl=en" class="social-icon social-instagram" title="Instagram" target="_blank"><i
-                         class="icon-instagram"></i></a>
+                 <a href="https://www.instagram.com/bobbin513/?hl=en" class="social-icon social-instagram"
+                     title="Instagram" target="_blank"><i class="icon-instagram"></i></a>
                  {{-- <a href="#" class="social-icon social-youtube" title="Youtube" target="_blank"><i
                          class="icon-youtube"></i></a> --}}
                  {{-- <a href="#" class="social-icon social-pinterest" title="Pinterest" target="_blank"><i
--- resources/views/frontend/pages/product-extended/sections/product-details.blade.php
+++ resources/views/frontend/pages/product-extended/sections/product-details.blade.php
@@ -1,6 +1,6 @@
 @php
 if($message = Session::get('success')){
-    toast($message,'success');
+toast($message,'success');
 }
 @endphp
 <div class="page-content">
@@ -50,7 +50,7 @@ if($message = Session::get('success')){
 
                         <div class="ratings-container">
                             <div class="ratings">
-                                <div class="ratings-val" style="width: 80%;"></div><!-- End .ratings-val -->
+                                <div class="ratings-val" style="width: 25%;"></div><!-- End .ratings-val -->
                             </div><!-- End .ratings -->
                             <a class="ratings-text" href="#product-review-link" id="review-link">( 2 Reviews )</a>
                         </div><!-- End .rating-container -->
@@ -131,61 +131,9 @@ if($message = Session::get('success')){
                                 <a
                                     href="{{ route('category',$product->category_id) }}">{{ $product->category->title }}</a>
                             </div><!-- End .product-cat -->
-                            
-                        </div><!-- End .product-details-footer -->
-                            <div class="col-sm-12">
-                <h2 class="title mb-1">Your Review About This Product</h2><!-- End .title mb-2 -->
-                {{-- <p class="mb-2">Use the form below to get in touch with the sales team</p> --}}
 
-            
-                <form action="{{ route('contact.store') }}" method="POST">
-                    @csrf
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div class="single-acc-field hidden">
-                                <label for="name">Name</label>
-                                <input class="form-control @error('name') is-invalid @enderror" type="text"
-                                    placeholder="Name" value="{{old('name')}}" name="name" id="name" required >
-                                @error('name')
-                                <div class="alert alert-danger">{{ $message }}</div>
-                                @enderror
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="single-acc-field hidden">
-                                <label for="email">Email</label>
-                                <input class="form-control @error('email') is-invalid @enderror" type="email"
-                                    value="{{old('email')}}" placeholder="Email" name="email" id="email" required>
-                                @error('email')
-                                <div class="alert alert-danger">{{ $message }}</div>
-                                @enderror
-                            </div>
-                          
-                        </div>
-                        <div class="col-lg-12">
-                            <div class="single-acc-field">
-                                {{-- <label for="msg">Message</label> --}}
-                                <textarea class="form-control @error('msg') is-invalid @enderror" name="msg" id="msg"
-                                    name="msg" value="{{old('msg')}}" rows="4" required></textarea>
-                                @error('msg')
-                                <div class="alert alert-danger">{{ $message }}</div>
-                                @enderror
-                            </div>
-                        </div>
-                    </div>
-                    {{-- <div class="single-acc-field boxes">
-                                        <input type="checkbox" id="checkbox">
-                                        <label for="checkbox">Remember me</label>
-                                    </div> --}}
-                    <div class="single-acc-field">
-                        <button type="submit" class="btn btn-outline-primary-2 btn-minwidth-sm">
-                            <span>SUBMIT</span>
-                            <i class="icon-long-arrow-right"></i>
-                        </button>
-                       
-                    </div>
-                </form>
-            </div><!-- End .col-lg-6 -->
+                        </div><!-- End .product-details-footer -->
+                      
                     </div><!-- End .product-details -->
                 </div><!-- End .col-md-6 -->
             </div><!-- End .row -->
